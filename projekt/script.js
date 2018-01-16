7ed2831b87de61bc0738d6680d3e13adbb18bdf5
Nifilym <34552046+Nifilym@users.noreply.github.com>
2018-01-16 08:03:00 +0100

    Add files via upload

--- projekt/script.js
+++ projekt/script.js
@@ -5,11 +5,12 @@ var stone = 15;
 var metal = 10;
 
 // Gebäudearten
-var gameObjects = ['farm', 'logger', 'stone', 'iron'];
-
+var gameObjects = ['farm', 'logger', 'stone', 'iron', 'Kaserne', 'Stallungen', 'Schießstand', 'Sklavenmarkt'];
+//Einheiten
 // Initial Level (wird später vom backend synchronisiert)
-var initialObjectLevel = {farm: 1, logger: 1, stone: 1, iron: 1};
-
+var initialObjectLevel = {farm: 1, logger: 1, stone: 1, iron: 1, Kaserne: 0, Stallungen: 0, Schießstand: 0, Sklavenmarkt: 0};
+//Einheitencounter
+//var unitNumbers = {Soldier: 0, Rider: 0, Archer: 0, Slavetroop: 0};
 // Gebäudekosten Start ---------------------------
 // Farm
 var farmgold = 10;
@@ -31,14 +32,101 @@ var irongold = 50;
 var ironwood = 20;
 var ironstone = 15;
 var ironmetal = 5;
+// Kaserne
+var Kasernegold = 300;
+var Kasernewood = 200;
+var Kasernestone = 150;
+var Kaserneiron = 100;
+//Stallungen
+var Stallungengold = 300;
+var Stallungenwood = 200;
+var Stallungenstone = 150;
+var Stallungeniron = 100;
+//Schießstand
+var Schießstandgold = 300;
+var Schießstandwood = 200;
+var Schießstandstone = 150;
+var Schießstandiron = 100;
+//Sklavenmarkt
+var Sklavenmarktgold = 300;
+var Sklavenmarktwood = 200;
+var Sklavenmarktstone = 150;
+var Sklavenmarktiron = 100;
 // Gebäudekosten Ende ----------------------------
+/*
+//Einheitenkosten pro Einheit
+
+//Soldat
+var Soldiergold = 20;
+var Soldierwood = 0;
+var Soldierstone = 0;
+var Soldieriron = 10;
+
+//Reiter
+var Ridergold = 50;
+var Riderwood = 0;
+var Riderstone = 0;
+var Rideriron = 30;
+
+//Bogenschütze
+var Archergold = 30;
+var Archerwood = 10
+var Archerstone = 0
+var Archeriron = 5
+
+//Sklavensoldat
+var Slavetroopgold = 5;
+var Slavetroopwood = 5;
+var Slavetroopstone = 0;
+var Slavetroopiron = 5;
+
+//Einheitenkostenende----------------------------
+*/
+//Resourcen Multiplikatoren
+var goldPerSecMult = 0.25;
+var woodPerSecMult = 0.25;
+var stonePerSecMult = 0.25;
+var metalPerSecMult = 0.25;
 
 // Resourcen pro Sekunde
-var goldPerSec = 1;
+var goldPerSec = 0.25;
 var woodPerSec = 0;
 var stonePerSec = 0;
 var metalPerSec = 0;
 
+//Forschungsarten
+var researchObjects = ['military', 'field', 'forest', 'stoneMine', 'ironMine'];
+
+//Forschungslevel
+var initialResearchLevel = {military: 0, field: 0, forest: 0, stoneMine: 0, ironMine: 0};
+
+//Forschungskosten Start ---------------------------
+//Militär
+var militaryGold = 10;
+var militaryWood = 10;
+var militaryStone = 10;
+var militaryMetal = 10;
+//Feld
+var fieldGold = 10;
+var fieldWood = 10;
+var fieldStone = 10;
+var fieldMetal = 10;
+//Wald
+var forestGold = 10;
+var forestWood = 10;
+var forestStone = 10;
+var forestMetal = 10;
+//Steinmine
+var stoneMineGold = 10;
+var stoneMineWood = 10;
+var stoneMineStone = 10;
+var stoneMineMetal = 10;
+//Eisenmine
+var ironMineGold = 10;
+var ironMineWood = 10;
+var ironMineStone = 10;
+var ironMineMetal = 10;
+//Forschungskosten Ende ---------------------------
 var gameData = {
 	objectCost: {
 		farm: {
@@ -65,20 +153,125 @@ var gameData = {
 			stone: ironstone,
 			metal: ironmetal,
 		},
+		Kaserne: {
+			gold: Kasernegold,
+			wood: Kasernewood,
+			stone: Kasernestone,
+			metal: Kaserneiron,
+		},
+		Stallungen: {
+			gold: Stallungengold,
+			wood: Stallungenwood,
+			stone: Stallungenstone,
+			metal: Stallungeniron,
+		},
+		Schießstand: {
+			gold: Schießstandgold,
+			wood: Schießstandwood,
+			stone: Schießstandstone,
+			metal: Schießstandiron,
+		},
+		Sklavenmarkt: {
+			gold: Sklavenmarktgold,
+			wood: Sklavenmarktwood,
+			stone: Sklavenmarktstone,
+			metal: Sklavenmarktiron,
+		},
 	},
+
+/*	unitCost: {
+		Soldier: {
+			gold: Soldiergold,
+			wood: Soldierwood,
+			stone: Soldierstone,
+			metal: Soldieriron,
+		},
+
+		Rider: {
+			gold: Ridergold,
+			wood: Riderwood,
+			stone: Riderstone,
+			metal: Rideriron,
+		},
+
+		Archer: {
+			gold: Archergold,
+			wood: Archerwood,
+			stone: Archerstone,
+			metal: Archeriron,
+		},
+
+		Slavetroop: {
+			gold: Slavetroopgold,
+			wood: Slavetroopwood,
+			stone: Slavetroopstone,
+			metal: Slavetroopiron,
+		},
+	},*/
+
 	objectLevel: {
 		farm: initialObjectLevel.farm,
 		logger: initialObjectLevel.logger,
 		stone: initialObjectLevel.stone,
 		iron: initialObjectLevel.iron,
+		Kaserne: initialObjectLevel.Kaserne,
+		Stallungen: initialObjectLevel.Stallungen,
+		Schießstand: initialObjectLevel.Schießstand,
+		Sklavenmarkt: initialObjectLevel.Sklavenmarkt,
 	},
 	playerResources: {
 		gold: gold,
 		wood: wood,
 		stone: stone,
 		metal: metal,
-	}
+	},
+
+	researchList: {
+		researchCost: {
+			military: {
+				gold: militaryGold,
+				wood: militaryWood,
+				stone: militaryStone,
+				metal: militaryMetal,
+			},
+			field: {
+				gold: fieldGold,
+				wood: fieldWood,
+				stone: fieldStone,
+				metal: fieldMetal,
+			},
+			forest: {
+				gold: forestGold,
+				wood: forestWood,
+				stone: forestStone,
+				metal: forestMetal,
+			},
+			stoneMine: {
+				gold: stoneMineGold,
+				wood: stoneMineWood,
+				stone: stoneMineStone,
+				metal: stoneMineMetal,
+			},
+			ironMine: {
+				gold: ironMineGold,
+				wood: ironMineWood,
+				stone: ironMineStone,
+				metal: ironMineMetal,
+			},
+		},
+		researchLevel: {
+		military: initialResearchLevel.military,
+		field: initialResearchLevel.field,
+		forest: initialResearchLevel.forest,
+		stoneMine: initialResearchLevel.stoneMine,
+		ironMine: initialResearchLevel.ironmine,
+		},
+	}	
 };
+//Gegner vars
+var enemySoldiers 	= 10;
+var soldiersRounded = enemySoldiers;
+
 
 $(document).ready(function () {
 
@@ -87,14 +280,28 @@ $(document).ready(function () {
 	//@todo Prüfen ob Spielstand vorhanden ist, ansonsten neuen erstellen
 	gameDataSync();
 
+	//Aktuelle RessourcenProduktion
+	calculateIncome();
+
 	// Derzeit verfügbare Rohstoffe
 	updateResources();
 
+	setInterval(function() {
+		enemySoldiers += (enemySoldiers/100)*1;
+		soldiersRounded = Math.round(enemySoldiers)
+	}, 1000);
+
 	// Rohstoff Legende
-	$( ".goldPerSec" ).append(goldPerSec);
-	$( ".stonePerSec" ).append(stonePerSec);
-	$( ".metalPerSec" ).append(metalPerSec);
-	$( ".woodPerSec" ).append(woodPerSec);
+	$( ".goldPerSec" ).append(4*goldPerSec);
+	$( ".stonePerSec" ).append(4*stonePerSec);
+	$( ".metalPerSec" ).append(4*metalPerSec);
+	$( ".woodPerSec" ).append(4*woodPerSec);
+
+	// Gegner Legende
+	$( ".enemyCount" ).append(soldiersRounded);
+
+	//Passives Gegnerverhalten
+	enemyPassive();
 
 	// Objektkosten
 	updateBuildCost()
@@ -108,7 +315,7 @@ $(document).ready(function () {
 		gameData.playerResources.stone = gameData.playerResources.stone + stonePerSec;
 		gameData.playerResources.metal = gameData.playerResources.metal + metalPerSec;
 		updateResources();
-	}, 1000);
+	}, 250);
 
 	
 	//Backend Synchronisation alle 3 Sekunden
@@ -157,6 +364,9 @@ function buyObject(type) {
 	//increase object level (später separate funktion)
 	gameData.objectLevel[type] += 1;
 	$( "#" + type ).find(".objectLevel").html(" ("+gameData.objectLevel[type]+") ");
+	
+	//Berechnung der Ressourcenproduktion
+	calculateIncome();
 
 	// Rohstoff Produktion ans neue Level anpassen
 	updateIncome(type);
@@ -169,26 +379,36 @@ function buyObject(type) {
 }
 
 function updateResources() {
-	$( ".gold" ).html(gameData.playerResources.gold);
-	$( ".wood" ).html(gameData.playerResources.wood);
-	$( ".stone" ).html(gameData.playerResources.stone);
-	$( ".metal" ).html(gameData.playerResources.metal);
+	$( ".gold" ).html(Math.ceil(gameData.playerResources.gold));
+	$( ".wood" ).html(Math.ceil(gameData.playerResources.wood));
+	$( ".stone" ).html(Math.ceil(gameData.playerResources.stone));
+	$( ".metal" ).html(Math.ceil(gameData.playerResources.metal));
+	$( ".enemyCount" ).html(soldiersRounded);
 }
 
 function decreaseResources(type) {
 	//decrease resources (später separate funktion)
-	gameData.playerResources.gold -= gameData.objectCost[type].gold;
-	gameData.playerResources.wood -= gameData.objectCost[type].wood;
-	gameData.playerResources.stone -= gameData.objectCost[type].stone;
-	gameData.playerResources.metal -= gameData.objectCost[type].metal;
+	gameData.playerResources.gold -= gameData.objectCost[type].gold //|| gameData.unitCost[type].gold;
+	gameData.playerResources.wood -= gameData.objectCost[type].wood //|| gameData.unitCost[type].wood;
+	gameData.playerResources.stone -= gameData.objectCost[type].stone //|| gameData.unitCost[type].stone;
+	gameData.playerResources.metal -= gameData.objectCost[type].metal //|| gameData.unitCost[type].metal;
 	updateResources();
 }
 
+//Berechnet die Ressourcenrate neu
+function calculateIncome() {
+	goldPerSec = goldPerSecMult*gameData.objectLevel.farm;
+	stonePerSec = stonePerSecMult*gameData.objectLevel.stone;
+	metalPerSec = metalPerSecMult*gameData.objectLevel.iron;
+	woodPerSec = woodPerSecMult*gameData.objectLevel.logger;
+}
+
+//Aktualisiert die visuelle Darstellung der Ressourcenrate
 function updateIncome(type) {
-	$( ".goldPerSec" ).html(goldPerSec);
-	$( ".stonePerSec" ).html(stonePerSec);
-	$( ".metalPerSec" ).html(metalPerSec);
-	$( ".woodPerSec" ).html(woodPerSec);
+	$( ".goldPerSec" ).html(goldPerSec*4);
+	$( ".stonePerSec" ).html(stonePerSec*4);
+	$( ".metalPerSec" ).html(metalPerSec*4);
+	$( ".woodPerSec" ).html(woodPerSec*4);
 }
 
 function checkResources(type) {
@@ -216,7 +436,7 @@ function updateBuildCost(type) {
 		// Baukosten
 		$( this ).find(".gold-value").html(gameData.objectCost[this.id].gold);
 		$( this ).find(".wood-value").html(gameData.objectCost[this.id].wood);
-		$( this ).find(".stone-value").html(gameData.objectCost[this.id].stone);
+		$( this ).find(".stone-value").html(gameData.objectCost[this.id].stone); 
 		$( this ).find(".metal-value").html(gameData.objectCost[this.id].metal);	
 
 		$( this ).find(".objectLevel").html(" ("+(gameData.objectLevel[this.id])+") ");
@@ -261,4 +481,46 @@ function gameDataSync() {
 	console.log(query);
 	console.log('Backend Synchronisation abgeschlossen');
 	return true;
+}
+
+function bauVisibility() {
+	$(bauMenu).css('display', 'inherit');
+	$(KriegMenu).css('display', 'none');
+	$(UnitMenu).css('display', 'none');
+}
+
+function kriegVisibility() {
+	$(KriegMenu).css('display', 'inherit');
+	$(bauMenu).css('display', 'none');
+	$(UnitMenu).css('display', 'none');
+}
+
+function unitVisibility() {
+	$(UnitMenu).css('display', 'inherit');
+	$(bauMenu).css('display', 'none');
+	$(KriegMenu).css('display', 'none');
+}
+
+function enemyPassive() {
+
+	x = true;
+	// Spawn und Verhalten nach Spawn
+	if (x == true) {enemySpawn();}
+	function enemySpawn() {
+
+		var leftRand = Math.floor((Math.random() * 90) + 1);
+		var leftSpawn = leftRand.toString();
+		leftSpawn += '%';
+
+		var topRand = Math.floor((Math.random() * 60) + 1);
+		var topSpawn = topRand.toString();
+		topSpawn += '%';
+
+		$(".enemyBase").addClass(" baseActive");
+		$('.enemyBase').css({ 'left': leftSpawn,'top':topSpawn })
+
+	
+
+	}
+
 }
